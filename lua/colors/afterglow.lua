a3673f9233b9b471639390980ca96636697b0ca1
bitterhalt <95308907+bitterhalt@users.noreply.github.com>
2024-01-25 01:06:23 +0200

    modified:   colors/base16-afterglow.vim 	modified:   lua/colors/afterglow.lua

--- lua/colors/afterglow.lua
+++ lua/colors/afterglow.lua
@@ -1,8 +1,8 @@
 return {
      base00 = '#212121', base01 = '#373737', base02 = '#4d4d4d', base03 = '#646464',
      base04 = '#7a7a7a', base05 = '#bcbcbc', base06 = '#a6a6a6', base07 = '#e9e9e9',
-     base08 = '#AC4142', base09 = '#dd7a1c', base0A = '#e5b567', base0B = '#7E8D50',
-     base0C = '#589591', base0D = '#6C9ABB', base0E = '#9e86c8', base0F = '#b05279'
+     base08 = '#AC4142', base09 = '#de935f', base0A = '#e5b567', base0B = '#7E8D50',
+     base0C = '#589591', base0D = '#6C9ABB', base0E = '#9e86c8', base0F = '#bd6768'
 }
 
 
